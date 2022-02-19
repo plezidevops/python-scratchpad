7683527d7b96dc25bd70885eacad546b53ce0f6e
pascalmlaurent@gmail.com <plaurent@pascal.jeremieonline.com>
2022-02-18 23:29:30 -0500

    for and while loop

---
+++ 5_0_for_loop.py
@@ -0,0 +1,12 @@
+for eachPass in range(4):
+    print("Wake up!", end=" ")
+
+# 10 is not included
+for number in range(5, 10):
+    print(number, end=' ')
+
+print('\n')
+
+# increment by the 3rd argument, 10 is not included
+for number in range(0, 10, 2):
+    print(number, end=' ')
---
+++ 5_2_while_loop.py
@@ -0,0 +1,11 @@
+count = 0
+while(count < 5):
+    print(count, end=" ")
+    count += 1
+
+print()
+
+i = 10
+while((i >= 0) == True):
+    print(i, end=" ")
+    i -= 1
