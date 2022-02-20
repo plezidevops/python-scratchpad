ddda4af9eb29c821bdec7cd7e9796ebfc80244ff
Plezi Devops <plezidevops@gmail.com>
2022-02-20 15:20:51 -0500

    find the maximum

---
+++ 7_2_maximum.py
@@ -0,0 +1,10 @@
+def maximum(value1, value2, value3):
+    '''Return the maximum of three values'''
+    max_value = value1
+    if value1 > max_value:
+        max_value = value1
+    if value2 > max_value:
+        max_value = value2
+    if value3 > max_value:
+        max_value = value3
+    return max_value
