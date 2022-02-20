e7a944218ff2af85668ce31c5710baa402d55cea
Plezi Devops <plezidevops@gmail.com>
2022-02-20 15:21:27 -0500

    defaiult and keyword parameters

---
+++ 8_default_parameter.py
@@ -0,0 +1,9 @@
+def rectangle_area(length=2, width=3):
+    '''Return a rectangle's area'''
+    return length * width
+
+print(rectangle_area())
+print(rectangle_area(3, 5))
+print(rectangle_area(10))
+
+print(rectangle_area(width=5, length=4))
---
+++ 9_keyword_arguments.py
@@ -0,0 +1,7 @@
+
+# each keyword argument in a call has the form parametervalue=value
+def rectangle_area(length, width):
+    '''Return a rectangle's area'''
+    return length * width
+
+print(rectangle_area(width=3, length=4))
